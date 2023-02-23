e51931a67e5469b5ee569c17bfd9a1c280948f81
Ned Rihine <ned.rihine@gmail.com>
2023-02-23 19:42:20 +0900

    nuget install BlazorStrap
    - のあと、BlazorStrap のセットアップ

--- FutureCentury.Tonzil/MauiProgram.cs
+++ FutureCentury.Tonzil/MauiProgram.cs
@@ -1,27 +1,27 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+﻿using BlazorStrap;
+
 using FutureCentury.Tonzil.Data;
 
 namespace FutureCentury.Tonzil;
 
-public static class MauiProgram
-{
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+public static class MauiProgram {
 
-		builder.Services.AddMauiBlazorWebView();
-		#if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+    public static MauiApp CreateMauiApp() {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts( fonts => {
+                fonts.AddFont( "OpenSans-Regular.ttf", "OpenSansRegular" );
+            } );
+
+        builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddBlazorStrap();
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
-		
-		builder.Services.AddSingleton<WeatherForecastService>();
 
-		return builder.Build();
-	}
+        builder.Services.AddSingleton<WeatherForecastService>();
+
+        return builder.Build();
+    }
 }
