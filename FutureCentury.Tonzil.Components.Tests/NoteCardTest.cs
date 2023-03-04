3e5292ed10e234a8f313d8f1fdd6a59289231e07
Ned Rihine <ned.rihine@gmail.com>
2023-03-04 17:26:13 +0900

    テストの名前変更

--- FutureCentury.Tonzil.Components.Tests/NoteCardTest.cs
+++ FutureCentury.Tonzil.Components.Tests/NoteCardTest.cs
@@ -5,7 +5,7 @@ namespace FutureCentury.Tonzil.Components.Tests;
 public class NoteCardTest : BunitTestContext {
 
     [Test]
-    public void ほげほげ() {
+    public void 要素が存在すること() {
         var aNote = new Note {
             Id = Guid.NewGuid().ToString( "N" ),
             Text = "レターパックで現金送れはすべて詐欺です。",
@@ -18,6 +18,8 @@ public class NoteCardTest : BunitTestContext {
         var founds = cut.FindAll( ".card .col-md-8 p.card-text" );
         Assert.That( founds, Has.Exactly( 2 ).Items );
         founds[0].MarkupMatches( $"<p class=\"card-text\">{aNote.Text}</p>" );
+
+        Assert.That( founds[1].Children, Has.Exactly( 1 ).Items );
         founds[1].Children[0].MarkupMatches( $"<small class=\"text-muted\">{aNote.CreatedAt}</small>" );
     }
 }
